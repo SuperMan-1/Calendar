4e4327f40c0b3a79f3c11616cca2738f9d08df55
SuperMan <SuperMan@DESKTOP-CNM2M86>
2019-04-12 18:30:38 +0900

    Update class methoe

--- src/My_Calendar.java
+++ src/My_Calendar.java
@@ -1,27 +1,41 @@
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+//import java.util.Calendar;
+//import java.util.GregorianCalendar;
 import java.util.Scanner;
 
 public class My_Calendar {
-	
-	public static void main(String[] args) {
-		
+
+	private static final int[] Max_Days = {31,28,31,30,31,30,31,31,30,31,30,31} ;
+
+    public static int GetMaxDaysOfMonth (int month) {
+
+    	return (Max_Days[month-1]) ;
+    	
+    }
+
+    public static void printcalendar() {
+
 		System.out.println("  일      월      화      수      목      금      토") ;
 		System.out.println("--------------------------") ;
 		System.out.println(" 1   2   3   4   5   6   7") ;
 		System.out.println(" 8   9  10  11  12  13  14") ;
 		System.out.println("15  16  17  18  19  20  21") ;
 		System.out.println("22  23  24  25  26  27  28") ;
+    }
+	
+	public static void main(String[] args) {
 
 		System.out.println("전체 날수를 구하고 싶은 년도와 월을 입력하세요") ;
 		Scanner scanner = new Scanner(System.in) ;
-		
+		My_Calendar calendar = new My_Calendar() ;
+
 		int month = scanner.nextInt() ;
 		scanner.close() ;
-		
-		int[] monthofdays = {31,28,31,30,31,30,31,31,30,31,30,31} ;
-		
-		System.out.printf("입력된 %d월의 날수는 %d 입니다. \n",month, monthofdays[month-1]) ;
+
+//		int days = GetMaxDaysOfMonth(month) ;
+
+    	System.out.printf("입력된 %d월의 날수는 %d 입니다. \n",month, calendar.GetMaxDaysOfMonth(month)) ;
+
+        calendar.printcalendar() ;
 
 //		int Year = scanner.nextInt() ;
 //		String str2 = scanner.next() ;
